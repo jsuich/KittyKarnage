9566733c22b907aebf8d3c19271f8accf8f47a7e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-22 18:59:12 -0500

    as good as its gonna get

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   // $("#5").hide();
-    $("#5").append("<img src='P1Right.png' width='125px' height='150px'/>");
-    $("#9").append("<img src='P2Left.png' width='125px' height='150px'/>");
+    $("#5").append("<img src='P1MusketRight.png' width='150px' height='150px'/>");
+    $("#9").append("<img src='P2FlamesLeft.png' width='150px' height='150px'/>");
 
     $('form').submit(function(event){
       event.preventDefault();
 
-      
+      for(var i = 0; i < 8; i++)
+      { movenum = i
+        player_one_move = 4
+      }
 
     });
 });
