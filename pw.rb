0bd680fed1baaf0638e80712def6abc09bb6e6f9
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-22 15:24:16 -0500

    updating almost final game logic

--- pw.rb
+++ pw.rb
@@ -1,10 +1,11 @@
 
 class Cat
 
-  def initialize(node, orientation)
+  def initialize(node, orientation, board)
     @node = node
     @coordinates = node.coordinates
     @orientation = orientation
+    @board = board
   end
 
   def rotate(direction)
@@ -26,37 +27,95 @@ class Cat
   end
 
   def walk
+
     @node.cat = nil
-    if @orientation == 1
-      @node = @board[@coordinates.first][@coordinates.last + 1]
-    elsif @orientation == 2
-      @node = @board[@coordinates.first]
-      @node.cat = self
+    if @orientation == 1 && @coordinates.last != @board.width-1 && @node.node_to_right.cat == nil
+      @node = @node.node_to_right
+    elsif @orientation == 2 && @coordinates.first != @board.height-1 && @node.node_below.cat == nil
+      @node = @node.node_below
+    elsif @orientation == 3 && @coordinates.last != 0 && @node.node_to_left.cat == nil
+      @node = @node.node_to_left
+    elsif @orientation == 4 && @coordinates.first != 0 && @node.node_above.cat == nil
+      @node = @node.node_above
+    end
 
-
+    @coordinates = @node.coordinates
+    @node.cat = self
 
   end
 
   def attack
+    if @orientation == 1 && @coordinates.last != @board.width-1
+      if @node.node_to_right.cat != nil
+         @node.node_to_right.cat = nil
+      end
+      if @node.node_to_right.node_to_right != nil && @node.node_to_right.node_to_right.cat != nil
+         @node.node_to_right.node_to_right.cat = nil
+      end
+    elsif @orientation == 2 && @coordinates.first != @board.height-1
+      if @node.node_below.cat != nil
+         @node.node_below.cat = nil
+      end
+      if @node.node_below.node_below != nil && @node.node_below.node_below.cat != nil
+         @node.node_below.node_below.cat = nil
+      end
+    elsif @orientation == 3 && @coordinates.last != 0
+      if @node.node_to_left.cat != nil
+         @node.node_to_left.cat = nil
+      end
+      if @node.node_to_left.node_to_left != nil && @node.node_to_left.node_to_left.cat != nil
+         @node.node_to_left.node_to_left.cat = nil
+      end
+    elsif @orientation == 4 && @coordinates.first != 0
+      if @node.node_above.cat != nil
+         @node.node_above.cat = nil
+      end
+      if @node.node_above.node_above != nil && @node.node_above.node_above.cat != nil
+         @node.node_above.node_above.cat = nil
+      end
+    end
   end
 
   def sleep
   end
+
+
+
 
 end
 
 class Node
   
   attr_accessor :contains_cat, :cat
+  attr_reader :coordinates
 
-  def initialize(coordinates)
+  def initialize(coordinates, board)
+    @board = board
     @coordinates = coordinates
     @cat = nil
+  end
+
+  def node_to_right
+    @board.board[@coordinates.first][@coordinates.last + 1]
+  end
+
+  def node_below
+    @board.board[@coordinates.first + 1][@coordinates.last]
+  end
+
+  def node_to_left
+    @board.board[@coordinates.first][@coordinates.last - 1]
+  end
+
+  def node_above
+    @board.board[@coordinates.first - 1][@coordinates.last]
   end
 
 end
 
 class Board
+
+  attr_reader :board, :width, :height
 
   def initialize(width, height)
     @width = width
@@ -69,23 +128,60 @@ class Board
     @board = Array.new(@height) {|array| array = Array.new}
     @board.each_with_index do |row, x_coordinate|
       @width.times do |y_coordinate|
-        row << Node.new([x_coordinate, y_coordinate])
+        row << Node.new([x_coordinate, y_coordinate], self)
       end
     end
   
   end
 
   def initialize_cats
-    @board[1][0].cat = New.cat(@board[1][0], 1)
-    @board[1][4].cat = New.cat(@board[1][4], 1)
+    @board[1][0].cat = Cat.new(@board[1][0], 1, self)
+    @board[1][4].cat = Cat.new(@board[1][4], 1, self)
+  end
+
+  def print_board
+    @board.each do |row|
+      row.each do |node|
+        if node.cat == nil
+          print " "
+        else
+          print "@"
+        end
+        print "|"
+      end
+      puts ""
+    end
   end
 
 end
 
 
-p Board.new(5,3)
-
-
-
+gameboard = Board.new(5, 3)
+gameboard.print_board
+puts ""
+gameboard.board[1][0].cat.walk
+gameboard.print_board
+puts ""
+gameboard.board[1][1].cat.walk
+gameboard.print_board
+puts ""
+gameboard.board[1][2].cat.walk
+gameboard.print_board
+puts ""
+gameboard.board[1][3].cat.walk
+gameboard.print_board
+puts ""
+gameboard.board[1][3].cat.rotate(:right)
+gameboard.board[1][3].cat.walk
+gameboard.print_board
+puts ""
+gameboard.board[2][3].cat.rotate(:left)
+gameboard.board[2][3].cat.walk
+gameboard.print_board
+puts ""
+gameboard.board[2][4].cat.rotate(:left)
+gameboard.board[2][4].cat.attack
+gameboard.print_board
+puts ""
 
 
