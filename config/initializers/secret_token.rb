42f0cf9fdbde61a31ca117db59749acade4f7785
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-22 13:10:09 -0500

    This is our first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KittyKarnage::Application.config.secret_token = 'c6e504b4b517388a3c22016c03f697e6490bd8ed3ccbff9ee233397b813543ef259cc3489c8a3f39909f71f66ab133aacee3084c3df88734cc4b0cecad5fea3c'
